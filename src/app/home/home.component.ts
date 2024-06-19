b486caafaeff73380bca24a540cbf25ce956456f
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-19 13:56:52 +0530

    "Updated home component with new styles, HTML structure, and button click handlers"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  contact(): void {
+    this.router.navigate(['/contact']);
+  }
+
+  about(): void {
+    this.router.navigate(['/about']);
+  }
+
+  list(): void {
+    this.router.navigate(['/list']);
+  }
 }
