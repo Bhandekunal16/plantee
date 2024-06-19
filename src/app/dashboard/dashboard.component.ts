ce50a7038fb292c1a9f237317cfc8b009948e582
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-19 11:34:02 +0530

    "Updated angular.json, added PrimeNG components to dashboard component, and modified styles.css"

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, ButtonModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  public item: any[] | undefined;
 
+  ngOnInit() {
+    this.item = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Project',
+        icon: 'pi pi-briefcase',
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-user',
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+      {
+        label: 'Package',
+        icon: 'pi pi-box',
+      },
+    ];
+  }
 }
