e85fa2a18cbd1791b521abc2f3d90fc12b82f8b1
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-19 16:16:51 +0530

    "Updated list component to display data in a PrimeNG table, added HTTP request to fetch data from API"

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, TableModule, CommonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  products!: any[];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.customerUnsubscribed().subscribe((ele) => {
+      this.products = ele.data;
+    });
+  }
+  customerUnsubscribed(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
 
+    return this.http
+      .get<any>(`https://townhall-ten.vercel.app/plants/species`, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
