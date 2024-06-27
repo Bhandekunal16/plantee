f971e0b9062313be450af2ac9fbf8c7263d0e7af
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-27 11:05:59 +0530

    "Added online status tracking to NetworkService using BehaviorSubject and window event listeners"

--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NetworkService {
+  private onlineStatus = new BehaviorSubject<boolean>(navigator.onLine);
+  onlineStatus$ = this.onlineStatus.asObservable();
 
-  constructor() { }
+  constructor() {
+    window.addEventListener('online', () => this.updateNetworkStatus(true));
+    window.addEventListener('offline', () => this.updateNetworkStatus(false));
+  }
+
+  private updateNetworkStatus(isOnline: boolean) {
+    this.onlineStatus.next(isOnline);
+  }
 }
